66e3b98a4fc1a6aac221acf5405e7aed81154d35
Văn Chiến <91095947+VAN-CHIEN@users.noreply.github.com>
2022-05-12 16:59:05 +0700

    .

--- main.js
+++ main.js
@@ -1,50 +1,137 @@
-var flower = function(name, money, quantity, images) {
-    this.flowerName = name;
-    this.flowerMoney =money;
-    this.flowerQuantity =quantity;
-    this.flowerImages =images;
-}
-var getdataSp =function(){
-    document.getElementById('nameSp').value= "";
-    document.getElementById('giaSp').value= "";
-    document.getElementById('soLuong').value= "";
-    document.getElementById('imagesSp').value= "";
-}
-var listSp = [];
+const infoId_nameHoa = document.getElementById('nameSp');
+const giaCa = document.getElementById('giaSp');
+const soLuongHoa = document.getElementById('soLuong');
+const linkHoa = document.getElementById('imagesSp');
+const addBtn = document.getElementById('submitSp'); 
+let list =[];
+addBtn.onclick =() => {
+    let obInfo = {
+       nameSP: infoId_nameHoa.value,
+       giaCa: giaCa.value,
+       soluong: soLuongHoa.value,
+       anh: linkHoa.value
+    }
+    var getdulieuSp =localStorage.getItem("infoSp");
+    if (getdulieuSp==null) {
+        let list =[];
 
-var thongkeSp =document.getElementById('showSp');
-var inputSp = function() {
-    var flSp = document.querySelectorAll('.nhaplieu input');
-    let st = new flower(flSp[0].value,flSp[1].value,flSp[2].value,flSp[3].value);
-    getdataSp();
-    listSp.push(st);
-    laydulieuSp();
+    } else {
+        let list =JSON.parse(getdulieuSp);
+    }
+    list.push(obInfo);
+    localStorage.setItem("infoSp",JSON.stringify(list));
+    xhinfoSp()
+    reset_form()
 }
-var laydulieuSp = function() {
-    var row ="";
-    for (var index= 0; index<listSp.length; index ++) {
+
+function xhinfoSp() {
+    var getdulieuSp =localStorage.getItem("infoSp");
+    if (getdulieuSp==null) {
+        list =[];
+
+    } else {
+        list =JSON.parse(getdulieuSp);
+    }
+    let row ="";
+    list.forEach((element,index) =>{
         row +=`
-        <div class="card" style="width: 18rem;margin-bottom: 10px;">
-                <img src=${listSp[index].flowerImages} class="card-img-top" alt="...">
-                <input type="button" value="x" id="deleteSp">
+        <div class="card" style="width: 15rem; margin-bottom: 10px;">
+                <p id="SpiD">${index}</p>
+                <img src="${element.anh}" class="card-img-top" alt="...">
+                <input type="button" value="x" class="deleteSp">
                 <div class="card-body">
-                    <h5 class="card-title"> ${listSp[index].flowerName}</h5>
-                    <p class="card-text">Hiện có: ${listSp[index].flowerQuantity}</p>
-                    <p class="card-text">Giá: ${listSp[index].flowerMoney} kVND</p>
-                    <input type="button" class="btn btn-primary" id="addGio" value="Thêm vào giỏ hàng">
+                    <h5 class="card-title">Tên sản phẩm:${element.nameSP}</h5>
+                    <p class="card-text">Số lượng:${element.soluong}</p>
+                    <p class="card-text">Giá sản phẩm:${element.giaCa}</p>
+                    <input type="button" class="btn btn-primary clickAdd" value="Thêm vào giỏ hàng">
                 </div>
             </div>
-        `;
-        localStorage.setItem(index,row);
-        var layra =localStorage.getItem(index);
-    }
-    var khunganh =document.getElementById('showSp');
-    khunganh.innerHTML=layra;
-    var spId = 1;
-    spId++;
-    var layngoai = localStorage.getItem("spId");
-    console.log(spId);
+        `
+    });
+    document.getElementById('showSp').innerHTML = row;
+    // DelSp = document.querySelectorAll('.deleteSp');
+    // console.log(DelSp);
+    // for (let u=0; u<DelSp.length;u++) {
+    //     DelSp[u].addEventListener("click",function() {
+    //         console.log(DelSp[u]);
+    //     });
+    // }
+        var btnGio =document.querySelectorAll('.clickAdd');
+        var dulieuSp =JSON.parse(localStorage.getItem("infoSp"));
+        var hot = "";
+        for (let y =0; y<btnGio.length;y++){
+            btnGio[y].addEventListener("click",function() {
+                for (z=0;z<dulieuSp.length;z++){
+                    if (y ==z) {
+                        hot+=`
+                            <br>
+                            <img src="${dulieuSp[z].anh}" width="50px" height="50px">
+                            <span>Tên:${dulieuSp[z].nameSP}</span>
+                            <div class="daNhap">
+                                <span>Số lượng:1</span>
+                                <span>Tổng giá:${dulieuSp[z].giaCa}</span>
+                            </div>
+                            <input type="button" value="Chỉnh sửa" class="editGio">
+                            <br>------------------
+                        `
+                    }
+                }
+                document.getElementById('giogio').innerHTML= hot;
+                var suaSp =document.querySelectorAll('.editGio');
+                var slGia =document.querySelectorAll('.daNhap');
+                for (let d =0;d<suaSp.length;d++){
+                    suaSp[d].addEventListener("click",function() {
+                        for (let f=0;f<slGia.length;f++){
+                            if (f==d) {
+                                slGia[f].innerHTML=`
+                            <div class="daNhap">
+                                <span>Số lượng:<input class="nhap1" type="number" size="10"></span>
+                                <span>Giá:<input class="nhap2" type="number" size="10"></span>
+                            </div>
+                            <input type="button" value="Xong" class="hoantat">
+                            `
+                            }
+                        
+                        };
+                        suaSp[d].style.display="none";
+                        var xong =document.querySelectorAll('.hoantat');
+                        var nhap1=document.querySelectorAll('.nhap1');
+                        var nhap2=document.querySelectorAll('.nhap2');
+                        for(let k=0;k<xong.length;k++) {
+                            xong[k].addEventListener("click",function() {
+                                suaSp[d].style.display="block";
+                                xong[k].style.display="none";
+                                for (let n =0; n<nhap1.length;n++){
+                                    for (let m =0; m<nhap2.length;m++){
+                                        for (let f=0;f<slGia.length;f++){
+                                                slGia[f].innerHTML=`
+                                            <div class="daNhap">
+                                                <span>Số lượng:${nhap1[n].value}</span>
+                                                <span>Giá:${nhap2[m].value}</span>
+                                            </div>
+                                            `
+                                        };
+                                    }
+                                }
+                            })
+                        }
+                        
+                    });
+                }
+            });
+        }
+};
+function reset_form() {
+    infoId_nameHoa.value = '';
+    giaCa.value = '';
+    soLuongHoa.value = '';
+    linkHoa.value = '';
+    infoId_nameHoa.focus();
 }
+window.onload = () => {
+    xhinfoSp();
+}
+//thêm sản phẩm vào giỏ
 
 // Xử lý modal nhập sản phẩm
 var modal = document.getElementById('keybaner');
@@ -64,7 +151,5 @@ chonGio =function () {
 }
 var nutxoa = document.getElementById('deleteSp');
 var nutaddSp =document.getElementById('submitSp');
-window.onload =function() {
-    nutaddSp.onclick = inputSp;
-}
+
 
